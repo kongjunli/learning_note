0659b746eb52658cb22289fd437a1d74409f133c
kongjunli <kongjunli@guazi.com>
2020-03-01 16:07:14 +0800

    code

--- 数据结构与算法/排序/code_java/selectSort.java
+++ 数据结构与算法/排序/code_java/selectSort.java
@@ -1,22 +1,14 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Arrays;
+
 public class selectSort {
-    public int[] getInput(int n){
-        Scanner sc=new Scanner(System.in);
-        int[] arr=new int[n];
-        System.out.println("input:");
-        for (int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
-        }
-        return arr;
-    }
     public void swap(int[] arr,int i,int j){
         int tmp=arr[i];
         arr[i]=arr[j];
         arr[j]=tmp;
     }
-    public int[] sortFuc(int[] arr){
+    public int[] sortFunc(int[] arr){
         for(int i=0;i<arr.length;i++){
             int index=i;
             for(int j=i;j<arr.length;j++){
@@ -28,14 +20,5 @@ public class selectSort {
 
         return arr;
     }
-    public static void main(String[] args){
-        selectSort ex=new selectSort();
-        int[] arr=ex.getInput(6);
-        int[] result=ex.sortFuc(arr);
-        for (int i=0;i<result.length;i++)
-            System.out.print(result[i]+" ");
-
-    }
-
 }
 
